c628f967ef09aa7ef8c4939df5e6c43b77179d21
Igor <itsdev74@gmail.com>
2022-04-05 13:55:32 +0400

    first

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const someVar = ref(0);
+const plusVar = () => {
+  someVar.value++;
+};
+</script>
+
+<template>
+  <div class="index">
+    {{someVar}}
+    <button @click="plusVar()">plus</button>
+  </div>
+</template>
