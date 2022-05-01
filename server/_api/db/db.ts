3260a344311289d941082120e661493ba17fdcd0
Igor <itsdev74@gmail.com>
2022-05-01 22:34:30 +0400

    test connection

--- server/_api/db/db.ts
+++ server/_api/db/db.ts
@@ -1,6 +1,12 @@
 import { MongoClient } from "mongodb";
+let client;
 
+console.log("---- TRY TO CONNECT ----");
 const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.0gezr.mongodb.net/?authMechanism=DEFAULT`;
-const client = new MongoClient(uri);
+try {
+  client = new MongoClient(uri);
+} catch (e: any) {
+  console.log("CONNECTING ERROR", e);
+}
 
 export default client;
