23cdb8d5dd81817cd03ca89bf19e1315c2777fd2
Igor <itsdev74@gmail.com>
2022-05-03 19:26:11 +0400

    vercel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -28,8 +28,8 @@ export default defineNuxtConfig({
     },
   },
   serverMiddleware: [
-    // { path: "/api", handler: "~/bin/middleware/server.js" },
-    // { path: "/api/**", handler: "~/bin/middleware/server.js" },
+    // { path: "/api", handler: "~/_api/server.js" },
+    // { path: "/api/**", handler: "~/_api/server.js" },
   ],
   typescript: {
     strict: true,
